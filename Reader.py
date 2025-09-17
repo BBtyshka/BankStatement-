0db87de3c38187d31004f9dbffc8d7186feddb49
Batyrkhan Tazhiyev <Batyshka123@gmail.com>
2025-09-17 12:40:57 +0800

    i

--- Reader.py
+++ Reader.py
@@ -1,1 +1,10 @@
-import camelot
+import camelot as cm
+
+tables = cm.read_pdf('eStatementFile_20250917122947.pdf')
+
+print(f'number of tables found: {len(tables)}')
+
+for i,table in enumerate(tables):
+    table.to_csv(f'tableN{i}.csv')
+
+    
